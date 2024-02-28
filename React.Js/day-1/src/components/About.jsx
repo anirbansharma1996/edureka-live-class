9b9fe9c5fbe4c61299f45c4be15d61d907849e10
Anirban <anirbansharma36@gmail.com>
2024-02-28 17:51:04 +0530

    28/02/2024 -[ first day of react class ] (#01)

---
+++ React.Js/day-1/src/components/About.jsx
@@ -0,0 +1,23 @@
+export default function About() {
+  return (
+    <>
+      <div className="main">
+        <div className="user-image">
+          <img
+            src="https://img.freepik.com/free-vector/black-white-spiral-background_1048-16005.jpg"
+            alt="user-image"
+          />
+        </div>
+        <div className="user-info">
+          <h3>Anirban</h3>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo
+            nesciunt voluptatum ipsa ab. Atque blanditiis voluptatem saepe
+            dolorem alias itaque distinctio et a incidunt commodi error, amet
+            ipsam expedita in.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
