a5ddb665ce693c527f85e88193327b743975a951
Anirban <anirbansharma36@gmail.com>
2024-03-27 17:41:15 +0530

    project-02 added

---
+++ React.Js/project-02/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./App.css"
+import Navbar from './components/navbar/Navbar'
+import Footer from './components/footer/Footer'
+
+export default function App() {
+  return (
+    <>
+      <Navbar/>
+       Main Content
+       <Footer/>
+    </>
+  )
+}
