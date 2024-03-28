e16c2ad70693e5784fcea4a091532e1becc67add
Anirban <anirbansharma36@gmail.com>
2024-03-28 17:30:18 +0530

    28/03/2024 - [ added routing , and hero section  ]

--- React.Js/project-02/src/App.js
+++ React.Js/project-02/src/App.js
@@ -1,14 +1,15 @@
-import React from 'react'
-import "./App.css"
-import Navbar from './components/navbar/Navbar'
-import Footer from './components/footer/Footer'
+import React from "react";
+import "./App.css";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import Allroutes from "./router/Allroutes";
 
 export default function App() {
   return (
     <>
-      <Navbar/>
-       Main Content
-       <Footer/>
+      <Navbar />
+      <Allroutes />
+      <Footer />
     </>
-  )
+  );
 }
--- React.Js/project-02/src/components/navbar/Navbar.jsx
+++ React.Js/project-02/src/components/navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Navbar.css";
 import logo from "../../assets/logo.jpeg";
+import {Link} from 'react-router-dom'
 
 export default function Navbar() {
   const links = [
@@ -12,33 +13,35 @@ export default function Navbar() {
     {
       p_name: "Products",
       p_icon: <i className="bi bi-list-task"></i>,
-      p_link: "/",
+      p_link: "/products",
     },
     {
       p_name: (
         <span class=" position-relative">
-         Cart
+          Cart
           <span class="position-absolute top-2 start-100 translate-middle badge rounded-pill bg-danger">
-           0
+            0
           </span>
         </span>
       ),
       p_icon: <i className="bi bi-cart2"></i>,
-      p_link: "/",
+      p_link: "/cart",
     },
     {
       p_name: "Login",
       p_icon: <i className="bi bi-person-lock"></i>,
-      p_link: "/",
+      p_link: "/login",
     },
   ];
 
   return (
     <nav>
-      <div className="nav-logo">
-        <img src={logo} alt="logo" />
-        <h3>.Shoppify</h3>
-      </div>
+      <Link style={{color:"black"}} to="/">
+        <div className="nav-logo">
+          <img src={logo} alt="logo" />
+          <h3>.Shoppify</h3>
+        </div>
+      </Link>
       <div className="nav-info">
         {links.map((el) => (
           <a href={el.p_link}>
