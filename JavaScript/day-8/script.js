afcff36e1f50ba497044629c04d30d749014e345
Anirban <anirbansharma36@gmail.com>
2024-04-09 18:51:32 +0530

    09/04/2024 - [ added dsa day-9 ](#01)

--- JavaScript/day-8/script.js
+++ JavaScript/day-8/script.js
@@ -1,7 +1,22 @@
-const date = new Date();
-
-console.log(date.getDate());
-console.log(date.getDay());
-console.log(date.getFullYear());
 
 
+// console.log(date.getDate());
+// console.log(date.getDay());
+// console.log(date.getFullYear());
+
+function Clock() {
+  setInterval(() => {
+    const date = new Date();
+    let hr = date.getHours();
+    let state ="AM"
+    const min = date.getMinutes();
+    const sec = date.getSeconds();
+
+    if(hr > 12 ){
+      hr = hr-12
+      state ="PM"
+    }
+    console.log(`Time is : ${hr}${state}:${min}min:${sec}secs `)
+  }, 1000);
+}
+Clock();
