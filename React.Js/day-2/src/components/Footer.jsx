772b8c02e1819d7cf59e0a8d2dbbd7456a74f5e7
Anirban <anirbansharma36@gmail.com>
2024-04-12 14:14:23 +0530

    12/04/2024 - [ added mongoDB note ](#01)

--- React.Js/day-2/src/components/Footer.jsx
+++
@@ -1,8 +0,0 @@
-export default function Footer(){
-    return(
-        <footer style={{textAlign:"center"}}>
-            <hr />
-            <h5>Designed by <span style={{color:"green"}}>Anirban Sharma</span> </h5>
-        </footer>
-    )
-}
--- React.Js/foody/README.md
+++
@@ -1,2 +0,0 @@
-1. axios
-2. BASE_URL 
