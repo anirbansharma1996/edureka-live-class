9b4dfe7d4e4295fea204ca070019dd980dfeb28b
Anirban <anirbansharma36@gmail.com>
2024-04-20 19:48:28 +0530

    20/04/2024 - [ added sorting - filtering  ] (#01)

--- batch-4/HTML + CSS + JS/day-26/script.js
+++ batch-4/HTML + CSS + JS/day-26/script.js
@@ -1,10 +1,30 @@
 const root = document.getElementById("root");
 const BASE_URL = "https://fakestoreapi.com/products";
+const sort = document.getElementById("sort-by-price");
+const filter_cat_1 = document.getElementById("category-1");
+const filter_cat_2 = document.getElementById("category-2");
+const filter_cat_3 = document.getElementById("category-3");
+const filter_cat_4 = document.getElementById("category-4");
+const reset = document.getElementById("reset-btn");
+// const mobile_sidebar_toggle = document.getElementById("mobile-sidebar-toggle");
+// const mobile_sidebar_inside = document.getElementById("mobile-sidebar-inside");
 
+// let flag = true;
+// mobile_sidebar_toggle.addEventListener("click", () => {
+//   if (flag === true) {
+//     mobile_sidebar_inside.style.display = "none";
+//   } else {
+//     mobile_sidebar_inside.style.display = "block";
+//   }
+//   flag = !flag;
+// });
+
+//
+let data;
 async function GetData() {
   try {
     const response = await fetch(BASE_URL);
-    const data = await response.json();
+    data = await response.json();
     ShowData(data);
   } catch (error) {
     console.log(error.message);
@@ -13,6 +33,11 @@ async function GetData() {
 GetData();
 
 function ShowData(arr) {
+  root.innerHTML = "";
+  document.getElementById(
+    "product-count"
+  ).innerText = `Product Count : ${arr.length}`;
+
   arr?.forEach((item) => {
     let main = document.createElement("div");
     main.className = "main";
@@ -41,7 +66,7 @@ function ShowData(arr) {
     rating.append(rate, count);
 
     let btn_div = document.createElement("div");
-    btn_div.className='buttons'
+    btn_div.className = "buttons";
     let cart_btn = document.createElement("button");
     cart_btn.innerText = "Add To Cart";
     let buy_btn = document.createElement("button");
@@ -53,10 +78,44 @@ function ShowData(arr) {
   });
 }
 
-// category -> span
-// description -> p
-// id -> alt
-// image -> img
-// price -> p
-// rating -> p
-// title -> h4
+// SORTING
+sort.addEventListener("change", (e) => {
+  if (e.target.value === "l-h") {
+    data = data.sort((a, b) => a.price - b.price);
+    ShowData(data);
+  } else {
+    data = data.sort((a, b) => b.price - a.price);
+    ShowData(data);
+  }
+});
+// FILTER
+filter_cat_1.addEventListener("click", (e) => {
+  let f_data = data.filter(
+    (el) => el.category === e.target.innerText.toLowerCase()
+  );
+  ShowData(f_data);
+});
+filter_cat_2.addEventListener("click", (e) => {
+  let f_data = data.filter(
+    (el) => el.category === e.target.innerText.toLowerCase()
+  );
+  ShowData(f_data);
+});
+filter_cat_3.addEventListener("click", (e) => {
+  let f_data = data.filter(
+    (el) => el.category === e.target.innerText.toLowerCase()
+  );
+
+  ShowData(f_data);
+});
+filter_cat_4.addEventListener("click", (e) => {
+  let f_data = data.filter(
+    (el) => el.category === e.target.innerText.toLowerCase()
+  );
+
+  ShowData(f_data);
+});
+//RESET
+reset.addEventListener("click", () => {
+  ShowData(data);
+});
