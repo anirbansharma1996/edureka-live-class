0e435f5e1c30b3361362889161c298576d503144
Anirban <anirbansharma36@gmail.com>
2024-04-23 18:21:05 +0530

    23/04/2024 - [ removed commentes ] (#02)

--- batch-3/Node.Js/day-2/fs/index.js
+++ batch-3/Node.Js/day-2/fs/index.js
@@ -1,39 +1,39 @@
 const fs = require("fs")
 
-// var rf = fs.readFileSync('data.txt','utf-8')
-// console.log(rf)
+var rf = fs.readFileSync('data.txt','utf-8')
+console.log(rf)
 
-// var text = 'hello i am creating a write file'
-// fs.writeFileSync("info.txt",text)
-// console.log('created')
+var text = 'hello i am creating a write file'
+fs.writeFileSync("info.txt",text)
+console.log('created')
 
-// var rf_2 = fs.readFileSync('data-2.txt','utf-8')
-// fs.writeFileSync('info-2.txt',rf_2)
-// console.log('created...')
+var rf_2 = fs.readFileSync('data-2.txt','utf-8')
+fs.writeFileSync('info-2.txt',rf_2)
+console.log('created...')
 
-//fs.unlinkSync('info-2.txt')
-//fs.unlinkSync('data-2.txt')
+fs.unlinkSync('info-2.txt')
+fs.unlinkSync('data-2.txt')
 
-//fs.mkdirSync('DIY')
-//fs.rmdirSync("DIY")
+fs.mkdirSync('DIY')
+fs.rmdirSync("DIY")
 
-// const rs = fs.createReadStream(__dirname+'/data.txt',"utf-8")
-// rs.on('open',()=>{
-//     console.log('file is open')
-// })
-// rs.on('data',(value)=>{
-//   console.log(value)
-// })
+const rs = fs.createReadStream(__dirname+'/data.txt',"utf-8")
+rs.on('open',()=>{
+    console.log('file is open')
+})
+rs.on('data',(value)=>{
+  console.log(value)
+})
 
-// const rs = fs.createReadStream(__dirname+"/data.txt",'utf-8')
-// const ws = fs.createWriteStream("write.txt")
+const rs1 = fs.createReadStream(__dirname+"/data.txt",'utf-8')
+const ws1 = fs.createWriteStream("write.txt")
 
-// rs.on('data',(value)=>{
-//      ws.write(value)
-// })
+rs1.on('data',(value)=>{
+     ws1.write(value)
+})
 
-var rs = fs.createReadStream(__dirname+"/data.txt","utf-8")
-const ws = fs.createWriteStream(__dirname+"/write.txt"
+var rs2 = fs.createReadStream(__dirname+"/data.txt","utf-8")
+const ws2 = fs.createWriteStream(__dirname+"/write.txt"
 )
-rs.pipe(ws)
+rs2.pipe(ws2)
 
--- batch-3/Node.Js/day-2/http/server.js
+++ batch-3/Node.Js/day-2/http/server.js
@@ -3,21 +3,21 @@ const http = require('http')
 const fs = require('fs')
 
 
-// const server = http.createServer((req,res)=>{
-//     res.writeHead(200,{"Content-Type":"text/plain"})
-//     res.end("hello this is my first server response")
-// })
+const server1 = http.createServer((req,res)=>{
+    res.writeHead(200,{"Content-Type":"text/plain"})
+    res.end("hello this is my first server response")
+})
 
-// server.listen(8000,'127.0.0.1')
-// console.log('http://127.0.0.1:8000')
+server1.listen(8000,'127.0.0.1')
+console.log('http://127.0.0.1:8000')
 
 
-const server = http.createServer((req,res)=>{
+const server2 = http.createServer((req,res)=>{
     res.writeHead(200,{'Content-Type':'text/plain'})
     const rs = fs.createReadStream(__dirname+"/data.txt","utf-8")
     rs.pipe(res) 
 })
 
-server.listen(8001,'127.0.0.1')
+server2.listen(8001,'127.0.0.1')
 console.log('http://127.0.0.1:8001')
 
